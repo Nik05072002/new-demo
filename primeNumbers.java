282267069b7e6942638e27edc5cf3c08dacaa97a
Nikhil Jadhao <nikhiljadhaofficial@gmail.com>
2023-10-02 00:49:00 +0530

    new

--- primeNumbers.java
+++ primeNumbers.java
@@ -1,22 +1,34 @@
-package JAVA;
+import java.util.*;
 
-public class primeNumbers{    
- public static void main(String args[]){    
-  int i,m=0,flag=0;      
-  int n=3;//it is the number to be checked    
-  m=n/2;      
-  if(n==0||n==1){  
-   System.out.println(n+" is not prime number");      
-  }else{  
-   for(i=2;i<=m;i++){      
-    if(n%i==0){      
-     System.out.println(n+" is not prime number");      
-     flag=1;      
-     break;      
-    }      
-   }      
-   if(flag==0)  { System.out.println(n+" is prime number"); }  
-  }//end of else  
-}    
-}   
+public class primeNumbers {
+
+	public static boolean isPrime(int n)
+	{
+		// Corner case
+		if (n <= 1)
+			return false;
+ 
+		// Check from 2 to n-1
+		for (int i = 2; i < n; i++)
+			if (n % i == 0)
+				return false;
+
+		return true;
+	}
+
+	// Driver Program
+	
+  public static void main(String args[])
+	{
+		if (isPrime(11))
+			System.out.println(" true");
+		else
+			System.out.println(" false");
+		if (isPrime(15))
+			System.out.println(" true");
+		else
+			System.out.println(" false");
+	}
+}
+
 
