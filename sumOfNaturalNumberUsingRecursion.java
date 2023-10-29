6a9b3130392436989f13a88bfec7fb42ba12c780
Nikhil Jadhao <nikhiljadhaofficial@gmail.com>
2023-10-29 21:35:57 +0530

    Java program to find the sum of Natural Numbers using Recursion.

---
+++ sumOfNaturalNumberUsingRecursion.java
@@ -0,0 +1,22 @@
+public class sumOfNaturalNumberUsingRecursion {
+
+
+  public static void main (String[]args)
+  {
+
+    int n = 10;
+    int sum = getSum (n);
+
+      System.out.println (sum);
+  }
+
+  static int getSum (int n)
+  {
+    if (n == 0)
+      return n;
+
+    return n + getSum (n - 1);
+  }
+}
+    
+
