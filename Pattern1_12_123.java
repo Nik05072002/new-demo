558a6b627e632fc445983d3cada2f105d5399bd7
Nikhil Jadhao <nikhiljadhaofficial@gmail.com>
2023-11-09 21:59:49 +0530

    Java Program to print the pattern 1 12 123 1234 12345  on the console

---
+++ Pattern1_12_123.java
@@ -0,0 +1,17 @@
+public class Pattern1_12_123 {
+
+        public static void main(String[] args)  
+        {  
+            int n = 5;  
+            for(int i = 0 ; i <= n ; i++)  
+            {  
+            for(int j = 0 ; j < i ; j++)  
+            {  
+                System.out.print(j+1);  
+            }  
+                System.out.println("");  
+            }  
+        }  
+      
+    
+}
