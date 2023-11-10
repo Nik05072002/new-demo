e8a42d490f7331c7ef7bf0abdd23b7c8fecf122e
Nikhil Jadhao <nikhiljadhaofficial@gmail.com>
2023-11-10 22:42:53 +0530

    Linear Search in Java

--- Pattern1_12_123.java
+++ Pattern1_12_123.java
@@ -1,3 +1,23 @@
+// Java program to print the following pattern on the console
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+// Algorithm:
+// STEP 1: START
+// STEP 2: SET n=5.
+// STEP 3: SET i=0. REPEAT STEP 4 to STEP 8 UNTIL i<=n.
+// STEP 4: SET j=0.REPEAT STEP 5 to STEP 6 UNTIL j
+// STEP 5: PRINT j+1
+// STEP 6: SET j=j+1
+// STEP 7: PRINT new line.
+// STEP 8: SET i=i+1.
+// STEP 9: END
+
+
+
 public class Pattern1_12_123 {
 
         public static void main(String[] args)  
